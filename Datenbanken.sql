87ab0094c4b0aec48fec9cd5ce431ab2bbefe686
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-04-16 20:16:27 +0200

    Setting up environment is done

---
+++ Datenbanken.sql
@@ -0,0 +1,32 @@
+﻿-- Database: "Datenbanken"
+
+-- DROP DATABASE "Datenbanken";
+
+CREATE DATABASE "Datenbanken"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+
+CREATE TABLE pizzen (
+	pizzaId	integer PRIMARY KEY,
+	pizzaName varchar(100) UNIQUE,
+	pizzaSize varchar(30) NOT NULL,
+	basePrice numeric(8,2) NOT NULL
+);
+
+INSERT INTO pizzen VALUES(1,'Pizza Salami Supreme, Classic','25 cm','4.70');
+INSERT INTO pizzen VALUES(2,'Pizza Tuna, Classic','25 cm','4.95');
+INSERT INTO pizzen VALUES(3,'Pizza Athena, Classic','25 cm','4.95');
+INSERT INTO pizzen VALUES(4,'Pizza Gruener Garten, Classic','25 cm','5.40');
+INSERT INTO pizzen VALUES(5,'Pizza Italia, Classic','25 cm','5.80');
+INSERT INTO pizzen VALUES(6,'Pizza Lucca, Classic','25 cm','5.80');
+INSERT INTO pizzen VALUES(7,'Pizza Mediterrana, Classic','25 cm','6.95');
+INSERT INTO pizzen VALUES(8,'Pizza Bombay, Classic','25 cm','5.80');
+INSERT INTO pizzen VALUES(9,'Pizza Dutchman, Classic','25 cm','5.80');
+INSERT INTO pizzen VALUES(10,'Pizza Olymp, Classic','25 cm','6.65');
+INSERT INTO pizzen VALUES(11,'Pizza Azzurri, Classic','25 cm','6.65');
+
+select * from pizzen;
---
+++ jdbcTest/src/jdbctest/JDBCTest.java
@@ -0,0 +1,74 @@
+package jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * *
+ * Small demo to show the use of jdbc in connection with postresql
+ *
+ * (C) 2017 TU Chemnitz / Datenverwaltungssysteme (DVS)
+ *
+ * @author richard vogel, johannes fliege
+ *
+ */
+public class JDBCTest
+{
+
+    /**
+     * Enter your Data here (Can be like exercise 1)
+     */
+    public static final String HOST = "localhost";
+    public static final int PORT = 5432;
+    public static final String DATABASENAME = "Datenbanken";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "1777537200";
+    public static final String TABLENAME = "pizzen";
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            // Load postgreSQL driver
+            Class.forName("org.postgresql.Driver");
+
+            String connectionUrl = "jdbc:postgresql://" + JDBCTest.HOST + ":" + JDBCTest.PORT + "/" + JDBCTest.DATABASENAME;
+
+            // 1. Establish connection
+            Connection connection = DriverManager.getConnection(connectionUrl, JDBCTest.USERNAME, JDBCTest.PASSWORD);
+
+            // 2. Create statement
+            Statement statement = connection.createStatement();
+
+            // 3. Execute statement
+            ResultSet result = statement.executeQuery("SELECT * FROM " + JDBCTest.TABLENAME);
+
+            // Read and print results
+            while (result.next())
+            {
+                for (int i = 1; i <= result.getMetaData().getColumnCount(); i++)
+                    System.out.println(result.getMetaData().getColumnLabel(i) + ": " + result.getString(i) + ";");
+                System.out.println();
+            }
+
+            // 4. Free ressources 
+            result.close();
+            statement.close();
+            connection.close();
+        }
+        catch (ClassNotFoundException e)
+        {
+            // Handle exception for not found database driver
+            System.out.println("JDBC-Driver postgres driver not found (Please add it to libraries)");
+        }
+        catch (SQLException e)
+        {
+            // Catch SQL Errors
+            System.out.println("Error while talking to database");
+            System.out.println(e.getMessage());
+        }
+    }
+}
